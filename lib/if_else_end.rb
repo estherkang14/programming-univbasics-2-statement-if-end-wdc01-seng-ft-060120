7728ceeace5e15c85fa6a0b8b761e4a451544f0c
Esther Kang <estherkang14@gmail.com>
2020-05-16 03:49:48 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,10 +1,8 @@
 # Write your solution here
 puts "Do you know what year it is?"
-this_year = 2023
-puts "It's 2020!" if this_year == 2020 
-else 
-  puts "No, sorry"
-end
+this_year = 2020
+puts "It's not 2020!" unless this_year == 2020
+
 
 
 
